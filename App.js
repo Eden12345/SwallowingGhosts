d818583d8cbd3aaccb4eb2793cb4a5ad66bf69ba
Eden <ethangooding@gmail.com>
2019-04-12 02:05:08 -0400

    Add styling

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, ScrollView, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo';
 
 import string1 from './1.js'
 import string2 from './14.js'
@@ -14,23 +15,26 @@ export default class App extends React.Component {
     const { view } = this.state;
     return (
       <View style={styles.container}>
-        {view === 1 && (
-          <ScrollView style={styles.scroller}>
-            <Text style={styles.text}>{string1}</Text>
-            <TouchableOpacity onPress={() => this.setState({ view: 2 })}>
-              <Text style={[styles.text, styles.bottomLink]}>{'Next >'}</Text>
-            </TouchableOpacity>
-            <Text style={styles.text}>hey</Text>
-          </ScrollView>
-        )}
-        {view === 2 && (
-          <ScrollView style={styles.scroller}>
-            <Text style={styles.text}>{string2}</Text>
-            <TouchableOpacity onPress={() => this.setState({ view: 1 })}>
-              <Text style={[styles.text, styles.bottomLink]}>{'< Back'}</Text>
-            </TouchableOpacity>
-          </ScrollView>
-        )}
+        <LinearGradient colors={['#404064', '#000']} start={[1, 0]} end={[0, 1]}>
+          <View style={styles.backdrop}>
+            {view === 1 && (
+              <ScrollView style={styles.scroller}>
+                <Text style={styles.text}>{string1}</Text>
+                <TouchableOpacity onPress={() => this.setState({ view: 2 })}>
+                  <Text style={[styles.text, styles.bottomLink]}>{'Next >'}</Text>
+                </TouchableOpacity>
+              </ScrollView>
+            )}
+            {view === 2 && (
+              <ScrollView style={styles.scroller}>
+                <Text style={styles.text}>{string2}</Text>
+                <TouchableOpacity onPress={() => this.setState({ view: 1 })}>
+                  <Text style={[styles.text, styles.bottomLink]}>{'< Back'}</Text>
+                </TouchableOpacity>
+              </ScrollView>
+            )}
+          </View>
+        </LinearGradient>
       </View>
     );
   }
@@ -43,8 +47,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  scroller: {
-    padding: 30,
+  backdrop: {
+    backgroundColor: 'rgba(0, 0, 0, .3)',
+    margin: 15,
+    marginTop: 40,
+    padding: 25,
   },
   text: {
     color: '#fff',
@@ -52,6 +59,5 @@ const styles = StyleSheet.create({
   },
   bottomLink: {
     textAlign: 'center',
-    marginBottom: 100,
   }
 });
