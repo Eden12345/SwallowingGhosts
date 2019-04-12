e896c45c58eb0ed1113c4f96528c17f15e58eab8
Eden <ethangooding@gmail.com>
2019-04-12 00:39:59 -0400

    Add first journal entry to project

---
+++ App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, ScrollView, Text, StyleSheet } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.scroller}>
+          <Text style={styles.text}>
+            {`\nThere were two tasks I had on my mind upon waking to another sunless sky; the first was to prove my sanity, and the second was to secure state funding for an interstellar voyage.\n\n
+
+            I woke in fright, with a hangover so intense it was practically spiritual. The campus, on which I worked and lived as temporary workshop facilitator, was a sterile network of interconnected lawns and poured concrete buildings, which would be crowded with the families of graduates. The day would not be an easy one. I felt around for a bag of water, and managed to locate it without lifting my spinning head from the pillow. Today will be difficult. I repeated this to myself, hoping the acknowledgement would absolve me of the previous night’s discretions.\n\n 
+
+            As a temporary workshop facilitator, I was not paid, but did receive one free meal in the cafeteria daily, as well as heavily subsidized campus housing. My apartment in SB-28, or sub basement floor 28, contained one folding table, two folding chairs (although I had no partner), a closet, and an inflatable mattress that, when deflated, could be easily stored in the closet. There were two communal bathrooms at each end of the hall, cluttered with 40 odd peoples toiletry necessities.\n\n
+
+            I enjoy some form of clutter. There’s something to be said for a desk covered in dusty papers with mismatched stationary. I’ve never found character in a hospital. Unfortunately, the lack of actual space anywhere on campus made clutter a hard fact. So, in my quarters, I built a series of shelves along the walls, from floor to ceiling, no more than one and a half feet in width. Each additional vertical/horizontal square foot acting as an expression of my truest self.\n\n
+
+            My workshop, Memories of Unlearned Behavior, carried no credits, and was, as a result of this, poorly attended. ‘There are threats of asteroids, ice caps turned to floodwaters,’ the school paper had written not long after my workshop began, ‘ and sentient A.I. turning vindictive….We’re supposed to be interested in how experiments...that involve flatworms eating other flatworms, have resulted in the canibal worms gaining sensory knowledge from the cannibalized worms?’\n\n
+
+            This was, obviously, a gross simplification of my first paper on the subject ‘Vitamin D Testing of the Flatworm: Electric Shock and Unlearned Behavior.’ By the start of my workshop I’d written five others, and had advanced beyond flatworms, to sea snails, mice, rats, primates, and unknown to most, humans.\n\n
+
+            The lab was only slightly larger than my apartment, and three days a week doubled as classroom for the workshop. This day would be one of those days, so I covered some of the tanks and locked away any notes that might contradict my first task, therefore greatly jeopardizing the second.\n\n\n\n\n`}
+          </Text>
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#877391',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scroller: {
+    padding: 30,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+  }
+});
